c70608b046a3517a9260ed725d8cd145330b419d
Орлов Ярослав <y.orlov@fonmix.ru>
2020-03-06 20:05:28 +0300

    'master: init commit Пт мар 6 20:05:28 MSK 2020'

--- project/lib/MyApp/Controller/Root.pm
+++ project/lib/MyApp/Controller/Root.pm
@@ -1,45 +1,28 @@
 package MyApp::Controller::Root;
 use Moose;
 use namespace::autoclean;
+use utf8;
 
 BEGIN { extends 'Catalyst::Controller' }
 
-#
-# Sets the actions in this controller to be registered with no prefix
-# so they function identically to actions created in MyApp.pm
-#
 __PACKAGE__->config(namespace => '');
-
-=encoding utf-8
-
-=head1 NAME
-
-MyApp::Controller::Root - Root Controller for MyApp
-
-=head1 DESCRIPTION
-
-[enter your description here]
-
-=head1 METHODS
-
-=head2 index
-
-The root page (/)
-
-=cut
 
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
-
-    # Hello World
-    $c->response->body( $c->welcome_message );
+    # $c->res->headers->{'Content-Type'} = 'application/json; charset=utf-8';
+    $c->response->body(qq{
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Купить беляши в москве</title>
+</head>
+<body>
+    <h2>Купить беляши в москве</h2>
+</body>
+</html>
+    });
 }
-
-=head2 default
-
-Standard 404 error page
-
-=cut
 
 sub default :Path {
     my ( $self, $c ) = @_;
@@ -47,24 +30,7 @@ sub default :Path {
     $c->response->status(404);
 }
 
-=head2 end
-
-Attempt to render a view, if needed.
-
-=cut
-
 sub end : ActionClass('RenderView') {}
-
-=head1 AUTHOR
-
-avis,,,
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
 
 __PACKAGE__->meta->make_immutable;
 
